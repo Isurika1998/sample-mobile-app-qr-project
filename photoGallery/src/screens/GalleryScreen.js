8deb687ac1c0e4aa9e0222ef7cbb105b33312c66
Isurika Perera <2018is055@stu.ucsc.cmb.ac.lk>
2022-01-05 15:16:26 +0530

    Initial commit

---
+++ photoGallery/src/screens/GalleryScreen.js
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React from 'react';
+import { useAuthContext } from "@asgardeo/auth-react-native";
+import { useLoginContext } from "../../context/LoginContext";
+import {View, StyleSheet, StatusBar} from 'react-native';
+import GridImageView from 'react-native-grid-image-viewer';
+
+const GalleryScreen = ({ navigation }) => {
+
+    const { loginState, setLoginState, loading, setLoading } = useLoginContext();
+    const { state, signOut, refreshAccessToken } = useAuthContext();
+
+    /**
+     * This function will handle the refresh button click.
+     */
+    const handleRefreshtoken = async () => {
+
+        setLoading(true);
+        refreshAccessToken()
+            .catch((error) => {
+                setLoading(false);
+                // eslint-disable-next-line no-console
+                console.log(error);
+            });
+    };
+
+    /**
+     * This function will handle the sign out button click.
+     */
+    const handleSignOut = async () => {
+
+        setLoginState({
+            ...loginState, ...state, hasLogoutInitiated: true
+        });
+
+        signOut()
+            .catch((error) => {
+                setLoading(false);
+                // eslint-disable-next-line no-console
+                console.log(error);
+            });
+    };
+
+    return (
+    <>
+    <StatusBar barStyle={'dark-content'} />
+      <View style={styles.background}>
+      <GridImageView data={[ 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzqe5muqrlSXq3ZbIf5sRlU-VXoN3MNCUUhQ&usqp=CAU',
+       'https://image3.mouthshut.com/images/imagesp/925791837s.jpg' ,
+        'https://9to5mac.com/wp-content/uploads/sites/6/2016/11/15-touch-bar-macbook-pro-tips-and-tricks1.jpg?quality=82&strip=all' ,
+         'https://images.unsplash.com/photo-1593642532973-d31b6557fa68?ixid=MXwxMjA3fDF8MHxzZWFyY2h8MXx8c29mdHdhcmV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80' ]} />
+      </View>
+      </>
+    );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: 'black',
+    flex: 1
+  },
+});
+
+export default GalleryScreen;
---
+++ photoGallery/src/screens/QRScannerScreen.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React, {useEffect} from 'react';
+import {StyleSheet, StatusBar, Button, View, Text} from 'react-native';
+//import QRCodeScanner from 'react-native-qrcode-scanner';
+//import { RNCamera } from 'react-native-camera';
+
+
+    let onSuccess = (e) => {
+        console.log('Scanned: ', e.data);
+
+//        try {
+//            let account = new AccountsService();
+//            account
+//                .addAccount(
+//                    JSON.parse(e.data),
+//                    pushId,
+//                )
+//                .then((response) => {
+//                    let res = JSON.parse(response);
+//                    console.log('Add account response: ' + response);
+//                    if (res.res == 'OK') {
+//                        navigation.navigate('Add Success', res.data);
+//                    } else if (res.res == 'FAILED') {
+//                        navigation.navigate('Add Failed');
+//                    }
+//                });
+//        } catch (err) {
+//            console.log(err);
+//            navigation.navigate('Add Failed');
+//        }
+    };
+
+
+const QRScannerScreen = ({ navigation }) => {
+
+    return (
+
+                <QRCodeScanner
+                    onRead={onSuccess}
+                    showMarker={true}
+                    flashMode={RNCamera.Constants.FlashMode.off}
+                    cameraStyle={{
+                        marginTop: 10,
+                        height: 200,
+                        alignSelf: 'flex-end',
+                    }}
+                />
+
+    );
+};
+
+export default QRScannerScreen;
